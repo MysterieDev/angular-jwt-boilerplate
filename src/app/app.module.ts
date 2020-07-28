14c5c156774679f80c4c6f4406f46994f50a26fb
Mysteriedev <josefsle97@gmail.com>
2020-07-28 10:56:17 +0200

    use auth interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './commons/components/header/header.component';
@@ -11,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 import { MustMatchDirective } from './commons/components/directives/must-match.directive';
 import { ProfileModule } from './profile/profile.module';
 import { HomeComponent } from './commons/components/home/home.component';
+import { AuthInterceptor } from './auth.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,7 +29,9 @@ import { HomeComponent } from './commons/components/home/home.component';
     FormsModule,
     ProfileModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/profile/components/profile/profile.component.ts
+++ src/app/profile/components/profile/profile.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as jwt_decode from 'jwt-decode';
 import { AuthService } from '../../../commons/services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  username: string;
-  private isLoggedIn$;
-  constructor(private authService: AuthService) {}
+  userinfo: UserInfo;
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.isLoggedIn$ = this.authService.LoggedInSubject;
-    this.authService.isLoggedIn();
-
-    if (localStorage.getItem('id_token')) {
-      const tokenObj: any = jwt_decode(localStorage.getItem('id_token'));
-      this.username = tokenObj.username;
-    }
+    this.http
+      .get<UserInfo>('api/user/info')
+      .subscribe((res) => (this.userinfo = res));
   }
+}
+
+export interface UserInfo {
+  username: string;
+  email: string;
+  role: string;
 }
